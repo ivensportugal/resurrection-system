c205c999a3e10a46946c23ce20324cb5aefc6fe1
= <ivensportugal@me.com>
2017-04-01 11:39:30 -0400

    Start Remove Residents

---
+++ src/removeresidentform.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <title>Resurrection College System</title>
+  </head>
+
+  <body>
+    <h1>Find the Resident</h1>
+    <?php require('./findresident.php'); ?>
+  </body>
+
+</html>
