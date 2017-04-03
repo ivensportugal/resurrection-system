bfc50154026a5c8e69d7cdfdf56305fadf203441
= <ivensportugal@me.com>
2017-04-03 11:04:43 -0400

    Buttons ready

--- src/removeresidentform.php
+++ src/removeresidentform.php
@@ -1,3 +1,27 @@
+<?php
+
+  require('../function/php-db-function.php');
+  require('../function/validation.php'     );
+
+  $searchBy   = $searchByErr = "";
+  $givenName  = "";
+  $familyName = "";
+  $room       = "";
+
+  $error = false;
+
+  if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    if($_POST['findResident']) {
+      echo 'Find a Resident Now!';
+    } elseif($_POST['removeResident']) {
+      echo 'Remove a Resident Now!';
+    }
+
+  }
+
+?>
+
 <!DOCTYPE HTML>
 <html>
   <head>
@@ -6,8 +30,13 @@
   </head>
 
   <body>
-    <h1>Find the Resident</h1>
+    <h1>Remove a Resident</h1>
+    <h2>Find a Resident</h2>
     <?php require('./findresident.php'); ?>
+    <h2>Select a Resident</h2>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+      <input type="submit" name="removeResident" id="removeResident" value="Remove">
+    <form>
   </body>
 
 </html>
