a73bc91fc15c1a1db8fedbaf5e8a090461f76247
= <ivensportugal@me.com>
2017-04-05 10:47:59 -0400

    selection on search by criteria

--- src/removeresidentform.php
+++ src/removeresidentform.php
@@ -3,6 +3,7 @@
   require('../function/php-db-function.php');
   require('../function/validation.php'     );
 
+  $messageErr = "";
   $searchBy   = $searchByErr = "";
   $givenName  = "";
   $familyName = "";
@@ -12,8 +13,18 @@
 
   if($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    if($_POST['findResident']) {
-      echo 'Find a Resident Now!';
+    if($_POST['searchResident']) {
+
+      if($_POST['searchByName']){
+        echo 'Search By Name';
+      } elseif ($_POST['searchByRoom']) {
+        echo 'Search By Room';
+      } else {
+        echo 'Select one';
+        $searchByErr = 'Select one search criteria';
+      }
+
+
     } elseif($_POST['removeResident']) {
       echo 'Remove a Resident Now!';
     }
@@ -26,13 +37,15 @@
 <html>
   <head>
     <meta charset="utf-8"/>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
     <title>Resurrection College System</title>
   </head>
 
   <body>
     <h1>Remove a Resident</h1>
-    <h2>Find a Resident</h2>
-    <?php require('./findresident.php'); ?>
+    <h2>Search a Resident</h2>
+    <p><span class="error"><?php echo $messageErr; ?></span></p>
+    <?php require('./searchresident.php'); ?>
     <h2>Select a Resident</h2>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
       <input type="submit" name="removeResident" id="removeResident" value="Remove">
